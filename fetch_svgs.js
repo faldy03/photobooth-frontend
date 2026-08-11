903da6e46770c077c14b7df3317b612bfd3bc472
faldy03 <m.fauzan.faldy17@gmail.com>
2026-08-11 16:38:23 +0700

    chore: remove temporary validation script fetch_svgs.js

--- fetch_svgs.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-const url = 'https://cdn.jsdelivr.net/gh/hafidznoor/idn-finlogos@master/icons/shopee-pay.svg';
-
-https.get(url, (res) => {
-  console.log(`SHOPEE-PAY STATUS: ${res.statusCode}`);
-}).on('error', (err) => {
-  console.error('Error:', err.message);
-});
